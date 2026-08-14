a276cfab87b6f40fab4ed775030452f25860259f
hungnm-1225 <hung.nguyenmanh@dtt.vn>
2026-08-14 23:18:42 +0700

    Add files via upload

--- src/components/ResultViewer.tsx
+++ src/components/ResultViewer.tsx
@@ -122,7 +122,6 @@ export const ResultViewer: React.FC<ResultViewerProps> = ({ result, onReset }) =
           </div>
 
           {/* Quick Copy & Export Action Buttons */}
-          <br></br>
           <div className="flex items-center flex-wrap gap-2 shrink-0">
             {isVietnamese ? (
               /* Vietnamese video actions */
